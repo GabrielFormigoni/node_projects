503ff19f1842ba14069e7b3c49c334fffa174cc9
Gabriel Formigoni <bielformigoni@gmail.com>
2022-11-29 17:37:56 -0300

    Revert "Projeto 5 finalizado"
    This reverts commit 5b830c602763188b7c56efd345f0e2c69bb6cb83.

--- 05-JWT-Basics/starter/errors/index.js
+++
@@ -1,9 +0,0 @@
-const CustomAPIError = require("./custom-error");
-const BadRequestError = require("./bad-request");
-const UnauthenticatedError = require("./unautheticated");
-
-module.exports = {
-  CustomAPIError,
-  BadRequestError,
-  UnauthenticatedError,
-};
