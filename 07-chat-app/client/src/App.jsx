144180a75fa5c3f773d5b18f88a51cad58b769bb
Gabriel Formigoni <bielformigoni@gmail.com>
2022-11-30 17:23:48 -0300

    Adicionando as rotas

---
+++ 07-chat-app/client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Chat from './components/Chat.jsx';
+import Join from './components/Join.jsx';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+       <Routes>
+        <Route path="/" exact element={<Join/>}/>
+        <Route path="/chat" exact element={<Chat/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
