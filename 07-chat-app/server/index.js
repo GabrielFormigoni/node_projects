f7981a28313a5a3635c1d92bd291724c59708f24
Gabriel Formigoni <bielformigoni@gmail.com>
2022-12-01 15:50:36 -0300

    Configurações iniciais utilizando socket

--- 07-chat-app/server/index.js
+++ 07-chat-app/server/index.js
@@ -11,4 +11,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(router);
 
+io.on("connection", (socket) => {
+  console.log("New user connected!!!");
+
+  io.on("disconnection", () => console.log("User left!!!"));
+});
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
